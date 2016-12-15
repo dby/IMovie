d2db7ea3e4c8a8a1e51cdd8cb6d9c76893e4d545
dby <1052856576@qq.com>
2016-12-15 15:10:50 +0800

    bugfix

--- iMovie/Views/Cinecism/CinecismTableViewCell.swift
+++ iMovie/Views/Cinecism/CinecismTableViewCell.swift
@@ -75,27 +75,27 @@ class CinecismTableViewCell: UITableViewCell, Reusable {
         headerViewLayout()
         
         self.headerView.snp.makeConstraints { (make) in
-            make.left.equalTo(self.contentView).offset(15)
-            make.right.equalTo(self.contentView).offset(-15)
+            make.left.equalTo(self.contentView).offset(10)
+            make.right.equalTo(self.contentView).offset(-10)
             make.top.equalTo(self.contentView).offset(10)
             make.height.equalTo(UIConstant.SCREEN_WIDTH / 2)
         }
         self.cincismInfoLabel.snp.makeConstraints { (make) in
             make.top.equalTo(self.headerView.snp.bottom)
-            make.left.equalTo(self.contentView).offset(15)
-            make.right.equalTo(self.contentView).offset(-15)
+            make.left.equalTo(self.contentView).offset(10)
+            make.right.equalTo(self.contentView).offset(-10)
             make.height.equalTo(20)
         }
         self.cincismTitleLabel.snp.makeConstraints { (make) in
             make.top.equalTo(self.cincismInfoLabel.snp.bottom)
-            make.left.equalTo(self.contentView).offset(15)
-            make.right.equalTo(self.contentView).offset(-15)
+            make.left.equalTo(self.contentView).offset(10)
+            make.right.equalTo(self.contentView).offset(-10)
             make.height.equalTo(30)
         }
         self.cincismBriefLabel.snp.makeConstraints { (make) in
             make.top.equalTo(self.cincismTitleLabel.snp.bottom)
-            make.left.equalTo(self.contentView).offset(15)
-            make.right.equalTo(self.contentView).offset(-15)
+            make.left.equalTo(self.contentView).offset(10)
+            make.right.equalTo(self.contentView).offset(-10)
             make.bottom.equalTo(self.separateLineView.snp.top).offset(-30)
         }
         self.separateLineView.snp.makeConstraints { (make) in
@@ -176,27 +176,27 @@ class CinecismTableViewCell: UITableViewCell, Reusable {
         let moviePosterImage: UIImageView = UIImageView()
         moviePosterImage.contentMode = .scaleAspectFill
         moviePosterImage.clipsToBounds = true
-        moviePosterImage.backgroundColor = UIColor.yellow
+        //moviePosterImage.backgroundColor = UIColor.yellow
         return moviePosterImage
     }()
     // 电影名称
     internal lazy var movieNameLabel: UILabel = {
         let movieNameLabel: UILabel = UILabel()
-        movieNameLabel.backgroundColor = UIColor.red
+        //movieNameLabel.backgroundColor = UIColor.red
         movieNameLabel.font = UIFont.customFont_FZLTZCHJW(fontSize: 16)
         return movieNameLabel
     }()
     // 电影评分
     internal lazy var movieRatingLabel: UILabel = {
         let movieRatingLabel: UILabel = UILabel()
-        movieRatingLabel.backgroundColor = UIColor.orange
+        //movieRatingLabel.backgroundColor = UIColor.orange
         movieRatingLabel.font = UIFont.customFont_FZLTXIHJW(fontSize: 15)
         return movieRatingLabel
     }()
     // movieInfoLabel 导演/演员信息
     internal lazy var movieInfoLabel: UILabel = {
         let movieInfoLabel: UILabel = UILabel()
-        movieInfoLabel.backgroundColor = UIColor.brown
+        //movieInfoLabel.backgroundColor = UIColor.brown
         movieInfoLabel.font = UIFont.customFont_FZLTXIHJW(fontSize: 13)
         return movieInfoLabel
     }()
@@ -209,7 +209,7 @@ class CinecismTableViewCell: UITableViewCell, Reusable {
     // cincism info 谁评价的电影
     internal lazy var cincismInfoLabel: UILabel = {
         let cincismInfoLabel: UILabel = UILabel()
-        cincismInfoLabel.backgroundColor = UIColor.cyan
+        //cincismInfoLabel.backgroundColor = UIColor.cyan
         cincismInfoLabel.font = UIFont.customFont_FZLTXIHJW(fontSize: 11)
         cincismInfoLabel.textColor = UIColor.darkGray
         return cincismInfoLabel
@@ -217,14 +217,14 @@ class CinecismTableViewCell: UITableViewCell, Reusable {
     // cincism title
     internal lazy var cincismTitleLabel: UILabel = {
         let cincismTitleLabel = UILabel()
-        cincismTitleLabel.backgroundColor = UIColor.blue
+        //cincismTitleLabel.backgroundColor = UIColor.blue
         cincismTitleLabel.font = UIFont.customFont_FZLTZCHJW(fontSize: 16)
         return cincismTitleLabel
     }()
     // cincism brief
     internal lazy var cincismBriefLabel: UILabel = {
         let cincismBriefLabel: UILabel = UILabel()
-        cincismBriefLabel.backgroundColor = UIColor.green
+        //cincismBriefLabel.backgroundColor = UIColor.green
         cincismBriefLabel.font = UIFont.customFont_FZLTXIHJW(fontSize: 14)
         cincismBriefLabel.numberOfLines = 0
         return cincismBriefLabel
